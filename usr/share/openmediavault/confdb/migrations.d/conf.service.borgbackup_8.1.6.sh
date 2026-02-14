6e9ee19fed68a7042a3903a97a14d4b7fa6544bb
Aaron Murray <plugins@omv-extras.org>
2026-02-14 23:12:25 +0000

    remove noatime option since noatime is the default and wasn't being included in cron job anyway
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

---
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.borgbackup_8.1.6.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+. /etc/default/openmediavault
+. /usr/share/openmediavault/scripts/helper-functions
+
+xpath="/config/services/borgbackup/archives/archive"
+
+xmlstarlet sel -t -m "${xpath}" -v "uuid" -n ${OMV_CONFIG_FILE} |
+    xmlstarlet unesc |
+    while read uuid; do
+        if ! omv_config_exists "${xpath}[uuid='${uuid}']/noatime"; then
+            omv_config_delete "${xpath}[uuid='${uuid}']/noatime"
+        fi
+    done;
+
+exit 0
